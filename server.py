7fabb0a76c6cfb6bbc3d6bf93a2ecddcd512e398
shaanz <shaanz@gmail.com>
2020-06-02 11:27:42 +0800

    Update server.py

--- server.py
+++ server.py
@@ -5,12 +5,24 @@ import grpc
 
 import hellonerd_pb2
 import hellonerd_pb2_grpc
+import random
+import re
 
 
 class HelloNerd(hellonerd_pb2_grpc.HelloNerd):
 
     def SayHello(self, request, context):
-        return hellonerd_pb2.HelloReply(message='Hello, %s!' % request.name)
+        uname = str (request.name)
+        regex = re.compile('[@_!#$%^&*()<>?/\|}{~:=]')
+        if(regex.search(uname) == None):
+            print ('Responding to ' + uname)
+            messages = ['Where is my coffee?', 'Is this legal?', 'Don\'t drink and root', 'Credit card number 3438 0202 1052 1426 ', 'I want to go to a mall' ]
+            msg = uname +', ' + messages[random.randint(0,4)]
+            return hellonerd_pb2.HelloReply(message= msg ) 
+        else:
+            return hellonerd_pb2.HelloReply(message = 'I see you dont follow by rules')
+         
+            
 
 
 def serve():
@@ -19,8 +31,10 @@ def serve():
     server.add_insecure_port('[::]:50055')
     server.start()
     server.wait_for_termination()
+   
 
 
 if __name__ == '__main__':
     logging.basicConfig()
     serve()
+    print('Server is ready to receive request !!')
