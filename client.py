3a174ed841d32fdb678827ae7ecab7021924e863
shaanz <shaanz@gmail.com>
2020-06-02 11:25:13 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import logging
+import sys
 
 import grpc
 
@@ -8,10 +9,25 @@ import hellonerd_pb2_grpc
 
 
 def run():
-    with grpc.insecure_channel('localhost:50055') as channel:
+    try:
+        uname = sys.argv[1]
+        server = sys.argv[2]
+        port = sys.argv[3]
+        detonate = sys.argv [4]
+    except:
+        detonate = "no"
+        print('usage: python client.py username ip  port  --normal/--detonate ')
+        sys.exit(0)
+       
+
+    with grpc.insecure_channel(server+':'+port) as channel:
         stub = hellonerd_pb2_grpc.HelloNerdStub(channel)
-        response = stub.SayHello(hellonerd_pb2.HelloRequest(name='test'))
-    print("Greeter client received: " + response.message)
+        if (detonate == '--detonate'):
+            while True:
+                response = stub.SayHello(hellonerd_pb2.HelloRequest(name=uname))
+                print ("Nerd Says : " + response.message)
+        response = stub.SayHello(hellonerd_pb2.HelloRequest(name=uname))
+        print("Nerd Says: " + response.message)
 
 
 if __name__ == '__main__':
